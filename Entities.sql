d5a21cf944e325aa429656be430d0ead0b331bde
MoDerPo <90388353+moritz-t-w@users.noreply.github.com>
2022-09-25 12:16:35 +0200

    inline constraints

--- Entities.sql
+++ Entities.sql
@@ -12,32 +12,24 @@ USE Menschen
 
 CREATE TABLE Geschlecht
 (
-	-- Fields --
-
 	ID
 		UNIQUEIDENTIFIER
-		NOT NULL,
+		NOT NULL
+		CONSTRAINT PK_Geschlecht PRIMARY KEY,
 	Binär -- Binary or on a spectrum? Analog, you might say...
 		BIT
 		NOT NULL,
 	Name -- Attack Helicopter
 		NVARCHAR(MAX),
 	Anrede -- Herr and Frau happen to have the same length
-		NCHAR(4),
+		NCHAR(4)
+		CONSTRAINT CK_Geschlecht_Anrede CHECK (Anrede IN ('Herr', 'Frau')),-- No Anrede works too, just leave it out
 	Pronomen
 		NVARCHAR(3)
-		NOT NULL,
+		NOT NULL
+		CONSTRAINT CK_Geschlecht_Pronomen CHECK (Pronomen IN ('er', 'sie', 'er/sie')),
 	GrussEndung -- Sehr geehrte(r/s)
 		NVARCHAR(4),
-
-	-- Constraints --
-	CONSTRAINT PK_Geschlecht PRIMARY KEY
-		(ID),
-
-	CONSTRAINT CK_Geschlecht_Anrede CHECK -- No Anrede works too, just leave it out
-		(Anrede IN ('Herr', 'Frau')),
-	CONSTRAINT CK_Geschlecht_Pronomen CHECK -- er, sie, er/sie, es, german isn't so progressive
-		(Pronomen IN ('er', 'sie', 'er/sie'))
 )
 Go
 
@@ -47,19 +39,14 @@ Go
 
 CREATE TABLE Strasse
 (
-	-- Fields --
 
 	ID
 		UNIQUEIDENTIFIER
-		NOT NULL,
+		NOT NULL
+		CONSTRAINT PK_Strasse PRIMARY KEY,
 	Name
 		NVARCHAR(MAX)
 		NOT NULL,
-
-	-- Constraints --
-
-	CONSTRAINT PK_Strasse PRIMARY KEY
-		(ID)
 )
 Go
 
@@ -68,20 +55,13 @@ Go
 ---- Land ----
 
 CREATE TABLE Land
-(
-	-- Fields --
-	
-	ID
+(ID
 		UNIQUEIDENTIFIER
-		NOT NULL,
+		NOT NULL
+		CONSTRAINT PK_Land PRIMARY KEY,
 	Name -- No UNIQUE constraint, to avoid starting a war
 		NVARCHAR(MAX)
 		NOT NULL
-
-	-- Constraints --
-
-	CONSTRAINT PK_Land PRIMARY KEY
-		(ID)
 )
 Go
 
@@ -91,22 +71,16 @@ Go
 
 CREATE TABLE Ort
 (
-	-- Fields --
-
 	ID
 		UNIQUEIDENTIFIER
-		NOT NULL,
+		NOT NULL
+		CONSTRAINT PK_Ort PRIMARY KEY,
 	Name
 		NVARCHAR(MAX)
 		NOT NULL,
 	Land -- Are there adresses outside of countries...? I hope not
 		UNIQUEIDENTIFIER
 		NOT NULL,
-
-	-- Constraints --
-
-	CONSTRAINT PK_Ort PRIMARY KEY
-		(ID)
 )
 Go
 
@@ -116,11 +90,10 @@ Go
 
 CREATE TABLE Adresse
 (
-	-- Fields --
-
 	ID
 		UNIQUEIDENTIFIER
-		NOT NULL,
+		NOT NULL
+		CONSTRAINT PK_Adresse PRIMARY KEY,
 	Strasse
 		UNIQUEIDENTIFIER
 		NOT NULL,
@@ -133,11 +106,6 @@ CREATE TABLE Adresse
 	Ort
 		UNIQUEIDENTIFIER
 		NOT NULL,
-
-	-- Constraints --
-
-	CONSTRAINT PK_Adresse PRIMARY KEY
-		(ID)
 )
 Go
 
@@ -147,44 +115,32 @@ Go
 
 CREATE TABLE Mensch
 (
-	-- Fields --
-
 	ID
 		UNIQUEIDENTIFIER
-		NOT NULL,
+		NOT NULL
+		CONSTRAINT PK_Mensch PRIMARY KEY,
 	Vorname -- some cultures have no first names
 		NVARCHAR(MAX),
 	Name -- but you gotta have at least one name
 		NVARCHAR(MAX)
 		NOT NULL,
 	Geburtsdatum
-		DATE,
+		DATE
+		CONSTRAINT CK_Mensch_Geburtsdatum CHECK (Geburtsdatum < GETDATE()), -- When do you really becoma a human...?
 	Geschlecht
 		UNIQUEIDENTIFIER
-		NOT NULL,
+		NOT NULL
+		CONSTRAINT FK_Mensch_Geschlecht FOREIGN KEY REFERENCES Geschlecht(ID),
 	Adresse
 		UNIQUEIDENTIFIER
-		NOT NULL,
+		NOT NULL
+		CONSTRAINT FK_Mensch_Adresse FOREIGN KEY REFERENCES Adresse(ID),
 	EMail
 		NVARCHAR(254) -- that's fixed
 		NOT NULL,
 	Telefon
 		NVARCHAR (15) -- ITU-T recommendation E.164 alegedly says max 15 digits
-		NOT NULL,
-
-	-- Constraints --
-
-	CONSTRAINT PK_Mensch PRIMARY KEY
-		(ID),
-
-	CONSTRAINT FK_Mensch_Geschlecht FOREIGN KEY
-		(Geschlecht) REFERENCES Geschlecht (ID),
-	CONSTRAINT FK_Mensch_Adresse FOREIGN KEY
-		(Adresse) REFERENCES Adresse (ID),
-
-	CONSTRAINT CHK_Mensch_Geburtsdatum CHECK -- When do you really becoma a human...?
-		(Geburtsdatum < GETDATE()),
-	CONSTRAINT CHK_Mensch_Telefon CHECK -- ITU-T recommendation E.164 alegedly says max 15 digits
-		(LEN(Telefon) <= 15)
+		NOT NULL
+		CONSTRAINT CK_Mensch_Telefon CHECK (LEN(Telefon) <= 15),
 )
 Go
