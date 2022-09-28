0a1cc291455e61e84b4dfcf514b2eca300af5579
MoDerPo <90388353+moritz-t-w@users.noreply.github.com>
2022-09-28 11:37:12 +0200

    consistent formatting and casing

--- Entities.sql
+++ Entities.sql
@@ -8,31 +8,26 @@ GO
 USE Menschen
 
 
+
 ---- Geschlecht ----
 
 CREATE TABLE Geschlecht
 (
-	ID
-		UNIQUEIDENTIFIER
+	ID UNIQUEIDENTIFIER
 		NOT NULL
 		CONSTRAINT PK_Geschlecht PRIMARY KEY
 		CONSTRAINT DF_Geschlecht_ID DEFAULT NEWID(),
-	Binär -- Binary or on a spectrum? Analog, you might say...
-		BIT
+	Binär BIT -- Binary or on a spectrum? Analog, you might say...
 		NOT NULL,
-	Name -- Attack Helicopter
-		NVARCHAR(MAX),
-	Anrede -- Herr and Frau happen to have the same length
-		NCHAR(4)
+	Name NVARCHAR (MAX),-- Attack Helicopter
+	Anrede NCHAR(4)-- Herr and Frau happen to have the same length
 		CONSTRAINT CK_Geschlecht_Anrede CHECK (Anrede IN ('Herr', 'Frau')),-- No Anrede works too, just leave it out
-	Pronomen
-		NVARCHAR(3)
+	Pronomen NVARCHAR(3)
 		NOT NULL
 		CONSTRAINT CK_Geschlecht_Pronomen CHECK (Pronomen IN ('er', 'sie', 'er/sie')),
-	GrussEndung -- Sehr geehrte(r/s)
-		NVARCHAR(4),
+	GrussEndung NVARCHAR(4), -- Sehr geehrte(r/s)
 )
-Go
+GO
 
 
 
@@ -41,32 +36,28 @@ Go
 CREATE TABLE Strasse
 (
 
-	ID
-		UNIQUEIDENTIFIER
+	ID UNIQUEIDENTIFIER
 		NOT NULL
 		CONSTRAINT PK_Strasse PRIMARY KEY
 		CONSTRAINT DF_Strasse_ID DEFAULT NEWID(),
-	Name
-		NVARCHAR(MAX)
+	Name NVARCHAR(MAX)
 		NOT NULL,
 )
-Go
+GO
 
 
 
 ---- Land ----
 
 CREATE TABLE Land
-(ID
-		UNIQUEIDENTIFIER
+(ID	UNIQUEIDENTIFIER
 		NOT NULL
 		CONSTRAINT PK_Land PRIMARY KEY
 		CONSTRAINT DF_Land_ID DEFAULT NEWID(),
-	Name -- No UNIQUE constraint, to avoid starting a war
-		NVARCHAR(MAX)
+	Name NVARCHAR(MAX) -- No UNIQUE constraint, to avoid starting a war
 		NOT NULL
 )
-Go
+GO
 
 
 
@@ -74,20 +65,17 @@ Go
 
 CREATE TABLE Ort
 (
-	ID
-		UNIQUEIDENTIFIER
+	ID UNIQUEIDENTIFIER
 		NOT NULL
 		CONSTRAINT PK_Ort PRIMARY KEY
 		CONSTRAINT DF_Ort_ID DEFAULT NEWID(),
-	Name
-		NVARCHAR(MAX)
+	Name NVARCHAR(MAX)
 		NOT NULL,
-	Land -- Are there adresses outside of countries...? I hope not
-		UNIQUEIDENTIFIER
+	Land UNIQUEIDENTIFIER -- Are there adresses outside of countries...? I hope not
 		NOT NULL
 		CONSTRAINT FK_Ort_Land FOREIGN KEY REFERENCES Land(ID)
 )
-Go
+GO
 
 
 
@@ -95,31 +83,24 @@ Go
 
 CREATE TABLE Adresse
 (
-	ID
-		UNIQUEIDENTIFIER
+	ID UNIQUEIDENTIFIER
 		NOT NULL
 		CONSTRAINT PK_Adresse PRIMARY KEY
 		CONSTRAINT DF_Adresse_ID DEFAULT NEWID(),
-	Strasse
-		UNIQUEIDENTIFIER
-
-NOT NULL
-CONSTRAINT FK_Adresse_Strasse FOREIGN KEY REFERENCES Strasse
-(ID),
-	Nummer -- worlds longest street number: 986039, TINYINT is not enough
-		INT
+	Strasse UNIQUEIDENTIFIER
+		NOT NULL
+		CONSTRAINT FK_Adresse_Strasse FOREIGN KEY REFERENCES Strasse(ID),
+	Nummer INT -- worlds longest street number: 986039, TINYINT is not enough
 		NOT NULL,
-	PLZ -- Feel free to make a table for those ¯\_(ツ)_/¯
-		NVARCHAR(10)
+	PLZ NVARCHAR(10) -- Feel free to make a table for those ¯\_(ツ)_/¯
 		NOT NULL,
-	Ort
-		UNIQUEIDENTIFIER
-
-NOT NULL
-CONSTRAINT FK_Adresse_Ort FOREIGN KEY REFERENCES Ort
-(ID),
+	Ort UNIQUEIDENTIFIER
+		NOT NULL
+		CONSTRAINT FK_Adresse_Ort FOREIGN KEY REFERENCES Ort (ID),
 )
-Go
+GO
+
+
 
 -- KrankheitsKategorie ----
 
@@ -133,6 +114,8 @@ CREATE TABLE KrankheitsKategorie
 		NOT NULL
 )
 GO
+
+
 
 ---- Krankheit ----
 
@@ -158,6 +141,8 @@ CREATE TABLE Krankheit
 )
 GO
 
+
+
 ---- Symptom ----
 
 CREATE TABLE Symptom
@@ -174,6 +159,9 @@ CREATE TABLE Symptom
 		NOT NULL
 )
 GO
+
+
+
 ---- Krankheit hat Symptom ----
 
 CREATE TABLE KrankheitSymptom
@@ -185,13 +173,11 @@ CREATE TABLE KrankheitSymptom
 	Symptom UNIQUEIDENTIFIER
 		NOT NULL
 		CONSTRAINT FK_KrankheitSymptom_Symptom REFERENCES Symptom (ID),
-
-
-CONSTRAINT PK_KrankheitSymptom
-PRIMARY KEY
-(Krankheit, Symptom)
+	CONSTRAINT PK_KrankheitSymptom PRIMARY KEY (Krankheit, Symptom)
 )
 GO
+
+
 
 ---- BeschaeftigungsKategorie ----
 
@@ -205,6 +191,8 @@ CREATE TABLE BeschaeftigungsKategorie
 		NOT NULL
 )
 GO
+
+
 
 ---- Beschaeftigung ----
 
@@ -222,41 +210,39 @@ CREATE TABLE Beschaeftigung
 		CONSTRAINT FK_Beschaeftigung_Kategorie REFERENCES BeschaeftigungsKategorie(ID)
 )
 GO
+
+
+
 ---- Mensch ----
 
 CREATE TABLE Mensch
 (
-	ID
-		UNIQUEIDENTIFIER
+	ID UNIQUEIDENTIFIER
 		NOT NULL
 		CONSTRAINT PK_Mensch PRIMARY KEY
 		CONSTRAINT DF_Mensch_ID DEFAULT NEWID(),
-	Vorname -- some cultures have no first names
-		NVARCHAR(MAX),
-	Name -- but you gotta have at least one name
-		NVARCHAR(MAX)
+	Vorname NVARCHAR(MAX), -- some cultures have no first names
+	Name NVARCHAR(MAX) -- but you gotta have at least one name
 		NOT NULL,
 	Geburtsdatum
 		DATE
 		NOT NULL
 		CONSTRAINT CK_Mensch_Geburtsdatum CHECK (Geburtsdatum < GETDATE()), -- When do you really becoma a human...?
-	Geschlecht
-		UNIQUEIDENTIFIER
+	Geschlecht UNIQUEIDENTIFIER
 		NOT NULL
 		CONSTRAINT FK_Mensch_Geschlecht FOREIGN KEY REFERENCES Geschlecht(ID),
-	Adresse
-		UNIQUEIDENTIFIER
+	Adresse UNIQUEIDENTIFIER
 		NOT NULL
 		CONSTRAINT FK_Mensch_Adresse FOREIGN KEY REFERENCES Adresse(ID),
-	EMail
-		NVARCHAR(254) -- that's fixed
+	EMail NVARCHAR(254) -- that's fixed
 		NOT NULL,
-	Telefon
-		NVARCHAR (15) -- ITU-T recommendation E.164 alegedly says max 15 digits
+	Telefon NVARCHAR (15) -- ITU-T recommendation E.164 alegedly says max 15 digits
 		NOT NULL
 		CONSTRAINT CK_Mensch_Telefon CHECK (LEN(Telefon) > 5)
 )
-Go
+GO
+
+
 
 ---- Mensch hat Beschaeftigung bei Arbeitgeber ----
 
@@ -283,6 +269,8 @@ CREATE TABLE MenschBeschaeftigungArbeitgeber
 )
 GO
 
+
+
 ---- Mensch hat Krankheit ----
 
 CREATE TABLE MenschKrankheit
@@ -294,15 +282,17 @@ CREATE TABLE MenschKrankheit
 	Krankheit UNIQUEIDENTIFIER
 		NOT NULL
 		CONSTRAINT FK_MenschKrankheit_Krankheit REFERENCES Krankheit (ID),
-	Diagnose
-		DATE
+	Diagnose DATE
 		NOT NULL,
 
 	CONSTRAINT PK_MenschKrankheit PRIMARY KEY (Mensch, Krankheit)
 )
 GO
 
+
+
 ---- Arzt -----
+
 CREATE TABLE Arzt
 (
 	ID UNIQUEIDENTIFIER
@@ -311,6 +301,8 @@ CREATE TABLE Arzt
 		CONSTRAINT FK_Arzt_Mensch FOREIGN KEY REFERENCES Mensch(ID)
 )
 GO
+
+
 
 ---- Mensch wird wegen Krankheit behandelt ----
 
@@ -326,22 +318,17 @@ CREATE TABLE Behandlung
 	Krankheit UNIQUEIDENTIFIER
 		NOT NULL
 		CONSTRAINT FK_Behandlung_Krankheit REFERENCES Krankheit (ID),
-	Datum
-		DATE
+	Datum DATE
 		NOT NULL,
-	Start
-		TIME
+	Start TIME
 		NOT NULL,
-	Ende
-		TIME
+	Ende TIME
 		NOT NULL,
-	Arzt
-		UNIQUEIDENTIFIER
+	Arzt UNIQUEIDENTIFIER
 		NOT NULL
 		CONSTRAINT FK_Behandlung_Arzt REFERENCES Arzt (ID)
 		CONSTRAINT CK_Behandlung_Arzt CHECK (Mensch <> Arzt),
-	Notiz
-		TEXT
+	Notiz TEXT
 		NOT NULL
 )
 GO
